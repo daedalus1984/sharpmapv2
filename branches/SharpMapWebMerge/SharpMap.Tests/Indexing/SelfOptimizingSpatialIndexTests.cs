eca22d31aab881c7cdfc9efd4eaabbe62501332e
codekaizen <codekaizen@d92b74e0-882c-0410-a55b-fb246dedd4db>
2008-09-05 01:49:58 +0000

    Branching to merge web

---
+++ branches/SharpMapWebMerge/SharpMap.Tests/Indexing/SelfOptimizingSpatialIndexTests.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+
+namespace SharpMap.Tests.Indexing
+{
+    [TestFixture]
+    public class SelfOptimizingSpatialIndexTests
+    {
+    }
+}
---
+++ branches/SharpMapWebMerge/SharpMap.Tests/Rendering/RasterRenderer2DTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using IVectorD = NPack.Interfaces.IVector<NPack.DoubleComponent>;
+using IMatrixD = NPack.Interfaces.IMatrix<NPack.DoubleComponent>;
+using ITransformMatrixD = NPack.Interfaces.ITransformMatrix<NPack.DoubleComponent>;
+
+namespace SharpMap.Tests.Rendering
+{
+
+    #region RasterRenderer2D
+
+    [TestFixture]
+    public class RasterRenderer2DTests
+    {
+    }
+
+    #endregion
+}
