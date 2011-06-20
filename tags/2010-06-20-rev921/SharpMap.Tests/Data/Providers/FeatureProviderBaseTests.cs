389d619be72f58c42630ff95004de7c78b967dbd
felix.obermaier@netcologne.de <felix.obermaier@netcologne.de@d92b74e0-882c-0410-a55b-fb246dedd4db>
2011-06-20 08:18:23 +0000

    [trunk (rev.921)] - tagging for further development

---
+++ tags/2010-06-20-rev921/SharpMap.Tests/Data/Providers/FeatureProviderBaseTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GeoAPI.Coordinates;
+using GeoAPI.CoordinateSystems.Transformations;
+using GeoAPI.Geometries;
+using GisSharpBlog.NetTopologySuite.Geometries;
+using NPack;
+using NPack.Interfaces;
+using NPack.Matrix;
+using ProjNet.CoordinateSystems.Transformations;
+using Rhino.Mocks;
+using SharpMap.Data.Providers;
+using Xunit;
+
+#if BUFFERED
+using NetTopologySuite.Coordinates;
+#else
+using BufferedCoordinate = NetTopologySuite.Coordinates.Simple.Coordinate;
+using BufferedCoordinateFactory = NetTopologySuite.Coordinates.Simple.CoordinateFactory;
+using BufferedCoordinateSequence = NetTopologySuite.Coordinates.Simple.CoordinateSequence;
+using BufferedCoordinateSequenceFactory = NetTopologySuite.Coordinates.Simple.CoordinateSequenceFactory;
+#endif
+
+namespace SharpMap.Tests.Data.Providers
+{
+    public class FeatureProviderBaseTests
+    {
+        [Fact]
+        public void CoordinateTransformationTransformsQueryHavingDifferentSpatialReferenceAsTransformationTarget()
+        {
+            MockRepository repository = new MockRepository();
+            FeatureProviderBase provider = repository.CreateMock<FeatureProviderBase>();
+            BufferedCoordinateFactory coordFactory = new BufferedCoordinateFactory();
+            BufferedCoordinateSequenceFactory coordSeqFactory = new BufferedCoordinateSequenceFactory(coordFactory);
+            IGeometryFactory<BufferedCoordinate> geoFactory = new GeometryFactory<BufferedCoordinate>(coordSeqFactory);
+            IMatrixFactory<DoubleComponent> matrixFactory = new LinearFactory<DoubleComponent>();
+            ICoordinateTransformationFactory transformFactory =
+                new CoordinateTransformationFactory<BufferedCoordinate>(coordFactory, geoFactory, matrixFactory);
+            SetupResult.For(provider.CoordinateTransformation).Return(transformFactory);
+        }
+
+        [Fact]
+        public void CoordinateTransformationDoesNotTransformsQueryHavingSameSpatialReferenceAsTransformationTarget()
+        {
+            
+        }
+    }
+}
---
+++ tags/2010-06-20-rev921/SharpMap.Tests/FixtureFactories.cs
@@ -0,0 +1,29 @@
+﻿using GeoAPI.Geometries;
+using GisSharpBlog.NetTopologySuite.Geometries;
+#if BUFFERED
+using NetTopologySuite.Coordinates;
+#else
+using BufferedCoordinate = NetTopologySuite.Coordinates.Simple.Coordinate;
+using BufferedCoordinateFactory = NetTopologySuite.Coordinates.Simple.CoordinateFactory;
+using BufferedCoordinateSequence = NetTopologySuite.Coordinates.Simple.CoordinateSequence;
+using BufferedCoordinateSequenceFactory = NetTopologySuite.Coordinates.Simple.CoordinateSequenceFactory;
+#endif
+
+namespace SharpMap.Tests
+{
+    public class FixtureFactories
+    {
+        private IGeometryFactory _geoFactory;
+
+        public FixtureFactories()
+        {
+            BufferedCoordinateSequenceFactory sequenceFactory = new BufferedCoordinateSequenceFactory();
+            _geoFactory = new GeometryFactory<BufferedCoordinate>(sequenceFactory);
+        }
+
+        public IGeometryFactory GeoFactory
+        {
+            get { return _geoFactory; }
+        }
+    }
+}
